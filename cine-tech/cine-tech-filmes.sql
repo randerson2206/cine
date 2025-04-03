f51b8ac3aa945c0620ba8f8e1a0f0afaa149d3cc
Randerson Souza <81870584+randerson2206@users.noreply.github.com>
2025-04-02 21:03:20 -0300

    Update cine-tech-filmes.sql

--- cine-tech/cine-tech-filmes.sql
+++ cine-tech/cine-tech-filmes.sql
@@ -1,16 +1,11 @@
-DROP TABLE IF EXISTS `filmes`;
-CREATE TABLE `filmes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(200) NOT NULL,
-  `sinopse` text DEFAULT NULL,
-  `capa` varchar(255) DEFAULT NULL,
-  `link` varchar(255) DEFAULT NULL,
-  `genero_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `genero_id` (`genero_id`),
-  CONSTRAINT `filmes_ibfk_1` FOREIGN KEY (`genero_id`) REFERENCES `generos` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-LOCK TABLES `filmes` WRITE;
-INSERT INTO `filmes` VALUES 
-UNLOCK TABLES;
+CREATE TABLE filmes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    sinopse TEXT NOT NULL,
+    capa VARCHAR(255) NULL,
+    link VARCHAR(255) NULL,
+    genero_id INT NULL,
+    data_lancamento DATE NOT NULL,
+    duracao INT NOT NULL,
+    FOREIGN KEY (genero_id) REFERENCES generos(id) ON DELETE SET NULL
+);
